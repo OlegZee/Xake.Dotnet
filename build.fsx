2d55426ed6c5d0ee6194738b3f82ffed4616351a
Oleg Zaimkin <olegzzzz@gmail.com>
2018-05-18 02:39:21 +0700

    refining build script

--- build.fsx
+++ build.fsx
@@ -8,40 +8,20 @@
 open Xake
 open Xake.Tasks
 
-let frameworks = ["netstandard2.0"; "net46"]
-let libtargets =
-    [ for t in frameworks do
-      for e in ["dll"; "xml"]
-        -> sprintf "out/%s/Xake.Dotnet.%s" t e
-    ]
-
 let getVersion () = recipe {
     let! verVar = getVar("VER")
-    let! ver = getEnv("VER")
-    return verVar |> Option.defaultValue (ver |> Option.defaultValue "0.0.1")
+    let! verEnv = getEnv("VER")
+    return verVar |> Option.defaultValue (verEnv |> Option.defaultValue "0.0.1")
 }
 
-let makePackageName () = recipe {
-    let! ver = getVersion()
-    let! verSuffix =
-        getVar("SUFFIX")
-        |> Recipe.map (
-            function
-            | None -> "-beta"
-            | Some "" -> "" // this is release!
-            | Some s -> "-" + s
-            )
+let makePackageName = sprintf "Xake.Dotnet.%s.nupkg"
 
-    return sprintf "Xake.Dotnet.%s%s.nupkg" ver verSuffix
-}
-
-let dotnet arglist = recipe {
-    do! shell {
+let dotnet arglist =
+    shell {
         cmd "dotnet"
         args arglist
         failonerror
         } |> Recipe.Ignore
-}
 
 do xakeScript {
     filelog "build.log" Verbosity.Diag
@@ -53,8 +33,12 @@ do xakeScript {
             do! need ["test"]
             }
 
-        "build" <== libtargets
         "clean" => rm {dir "out"}
+        "build" <==
+                [ for t in ["netstandard2.0"; "net46"] do
+                  for e in ["dll"; "xml"]
+                    -> sprintf "out/%s/Xake.Dotnet.%s" t e
+                ]
 
         "test" => recipe {
             do! alwaysRerun()
@@ -69,37 +53,39 @@ do xakeScript {
             do! dotnet <| ["test"; "tests"; "-c"; "Release"] @ where @ limitFwk
         }
 
-        libtargets *..> recipe {
+        [ "out/(fwk:*)/Xake.Dotnet.dll"
+          "out/(fwk:*)/Xake.Dotnet.xml"] *..> recipe {
 
-            let! allFiles
-                = getFiles <| fileset {
+            do! fileset {
                     basedir "src"
                     includes "Xake.Dotnet.fsproj"
                     includes "**/*.fs"
                 }
+                |> getFiles
+                |> Recipe.map needFiles
+                |> Recipe.Ignore
 
-            do! needFiles allFiles
+            let! framework = getRuleMatch "fwk"
             let! version = getVersion()
 
-            for framework in frameworks do
-                do! dotnet
-                        [
-                            "build"
-                            "src"
-                            "/p:Version=" + version
-                            "--configuration"; "Release"
-                            "--framework"; framework
-                            "--output"; "../out/" + framework
-                            "/p:DocumentationFile=Xake.Dotnet.xml"
-                        ]
+            do! dotnet
+                  [
+                    "build"
+                    "src"
+                    "/p:Version=" + version
+                    "--configuration"; "Release"
+                    "--framework"; framework
+                    "--output"; "../out/" + framework
+                    "/p:DocumentationFile=Xake.Dotnet.xml"
+                  ]
         }
     ]
 
     (* Nuget publishing rules *)
     rules [
         "pack" => recipe {
-            let! package_name = makePackageName ()
-            do! need ["out" </> package_name]
+            let! version = getVersion()
+            do! need ["out" </> makePackageName version]
         }
 
         "out/Xake.Dotnet.(ver:*).nupkg" ..> recipe {
@@ -116,18 +102,16 @@ do xakeScript {
 
         // push need pack to be explicitly called in advance
         "push" => recipe {
-
-            let! package_name = makePackageName ()
-
+            let! version = getVersion()
             let! nuget_key = getEnv("NUGET_KEY")
+
             do! dotnet
                   [
                     "nuget"; "push"
-                    "out" </> package_name
+                    "out" </> makePackageName version
                     "--source"; "https://www.nuget.org/api/v2/package"
                     "--api-key"; nuget_key |> Option.defaultValue ""
                   ]
         }
     ]
 }
-
